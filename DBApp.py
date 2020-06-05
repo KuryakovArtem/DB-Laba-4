daec0554cd99e34ecf6eea12eddb25fd85d56ee6
kolyanok <kolyan-okunok@ya.ru>
2020-06-05 17:38:55 +0300

    code cleanup + add records + delete records

--- DBApp.py
+++ DBApp.py
@@ -1,45 +1,72 @@
-from PyQt5.QtCore    import *
-from PyQt5.QtGui     import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5 import *
 import design
 import connectUI
 from DB import DB
 
+
 class connectWindow(QtWidgets.QMainWindow, connectUI.Ui_Connect):
-	 def __init__(self, dba):
-	 	super().__init__()
-	 	self.setupUi(self)
-	 	self.dba = dba
-	 def accept(self):
-	 	try:
-	 		self.dba.connectDB(self.host.text(), self.port.text(), self.login.text(), self.password.text(), self.name.text(), self.structure.text())
-	 	except Exception as e:
-	 		self.dba.errorMessage(str(e))
-	 	self.close()
-	 def reject(self):
-	 	self.close()
+    def __init__(self, dba):
+        super().__init__()
+        self.setupUi(self)
+        self.dba = dba
+
+    def keyPressEvent(self, qKeyEvent):
+        if qKeyEvent.key() == QtCore.Qt.Key_Return:
+            self.accept()
+
+    def accept(self):
+        try:
+            self.dba.connectDB(self.host.text(), self.port.text(),
+                               self.login.text(), self.password.text(),
+                               self.name.text(), self.structure.text())
+            self.close()
+        except Exception as e:
+            self.dba.errorMessage(str(e))
+
+    def reject(self):
+        self.close()
+
 
 class DBApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         self.db = None
         super().__init__()
         self.columnsAssortment = ['id', 'name', 'stock', 'price']
-        self.columnsOrders = ['id', 'item', 'quantity', 'creation_time', 'last_modification_time']
+        self.columnsOrders = [
+            'id', 'item', 'quantity', 'creation_time', 'last_modification_time'
+        ]
         self.setupUi(self)
         self.w = connectWindow(self)
-        self.actionConnect.triggered.connect(self.w.show)    
+        self.actionConnect.triggered.connect(self.w.show)
+        self.addButtonAssortment.clicked.connect(self.addRecordToAssortment)
+        self.addButtonOrders.clicked.connect(self.addRecordToOrders)
+        self.deleteButtonAssortment.clicked.connect(self.delAssortment)
+        self.deleteButtonOrders.clicked.connect(self.delOrders)
         self.tableWidgetAssortment.setColumnCount(4)
-        self.tableWidgetAssortment.setHorizontalHeaderLabels(self.columnsAssortment)
+        self.tableWidgetAssortment.setHorizontalHeaderLabels(
+            self.columnsAssortment)
         self.tableWidgetOrders.setColumnCount(5)
         self.tableWidgetOrders.setHorizontalHeaderLabels(self.columnsOrders)
-        
+        self.tabledataAssortment = None
+        self.tabledataOrders = None
+
     def connectDB(self, host, port, login, password, name, structureURL):
-    	self.db = DB(host, port, login, password, name, structureURL)
-    	print (self.db.getOrders())
-    	self.setDataToTable(self.columnsAssortment, self.tableWidgetAssortment, self.db.getAssortment())
-    	self.setDataToTable(self.columnsOrders, self.tableWidgetOrders, self.db.getOrders())
-    
+        self.db = DB(host, port, login, password, name, structureURL)
+        print(self.db.getOrders())
+        try:
+            self.tabledataAssortment = self.db.getAssortment()
+            self.setDataToTable(self.columnsAssortment,
+                                self.tableWidgetAssortment,
+                                self.tabledataAssortment)
+            self.tabledataOrders = self.db.getOrders()
+            self.setDataToTable(self.columnsOrders, self.tableWidgetOrders,
+                                self.tabledataOrders)
+        except Exception as e:
+            self.errorMessage(str(e))
+
     def updateData(self, item):
         if not self.settingdata:
             try:
@@ -56,14 +83,15 @@ class DBApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
             self.saved = False
 
     def setDataToTable(self, columns, table, data):
-        self.tabledata = data
+        if data is None or len(data) == 0:
+            table.setRowCount(0)
+            return
         self.settingdata = True
         table.setRowCount(len(data))
         for rownum, row in enumerate(data):
             for colnum, col in enumerate(columns):
                 table.setItem(rownum, colnum, QTableWidgetItem(str(row[col])))
         self.settingdata = False
-
 
     def errorMessage(self, error):
         dialog = QMessageBox()
@@ -72,20 +100,42 @@ class DBApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         dialog.addButton(QMessageBox.Ok)
         dialog.exec()
 
-    def addRecord(self):
-        id = self.id.text()
-        price = self.price.text()
-        amount = self.amount.text()
-        name = self.name.text()
-        if id.isnumeric() and price and amount and name:
+    def addRecordToAssortment(self):
+        if self.db is None:
+            self.errorMessage("The database is not connected")
+            return
+        if len(self.nameAssortment.text()) == 0 or len(
+                self.stockAssortment.text()) == 0 or len(
+                    self.priceAssortment.text()) == 0:
+            self.errorMessage("Not all fields are filled in")
+        else:
             try:
-                self.db.addRecord({"id": int(id), "name": name, "amount": amount, "price": price})
-            except Exception as error:
-                self.errorMessage(str(error))
-        self.settingdata = True
-        self.setDataToTable(self.db.getRecords())
-        self.settingdata = False
-        self.saved = False
+                self.db.addItem(self.nameAssortment.text(),
+                                self.stockAssortment.text(),
+                                self.priceAssortment.text())
+                self.tabledataAssortment = self.db.getAssortment()
+                self.setDataToTable(self.columnsAssortment,
+                                    self.tableWidgetAssortment,
+                                    self.tabledataAssortment)
+            except Exception as e:
+                self.errorMessage(str(e))
+
+    def addRecordToOrders(self):
+        if self.db is None:
+            self.errorMessage("The database is not connected")
+            return
+        if len(self.ItemOrders.text()) == 0 or len(
+                self.QuantityOrders.text()) == 0:
+            self.errorMessage("Not all fields are filled in")
+        else:
+            try:
+                self.db.addOrder(self.ItemOrders.text(),
+                                 self.QuantityOrders.text())
+                self.tabledataOrders = self.db.getOrders()
+                self.setDataToTable(self.columnsOrders, self.tableWidgetOrders,
+                                    self.tabledataOrders)
+            except Exception as e:
+                self.errorMessage(str(e))
 
     def search(self):
         text = self.nameSD.text()
@@ -97,11 +147,43 @@ class DBApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         except Exception as error:
             self.errorMessage(str(error))
 
-    def delByName(self):
-        name = self.nameSD.text()
+    def delAssortmentByName(self):
+        if self.db is None:
+            self.errorMessage("The database is not connected")
+            return
+        name = self.nameSAssortment.text()
+        if (name):
+            try:
+                self.db.deleteByName(name)
+                self.tabledataAssortment = self.db.getAssortment()
+                self.setDataToTable(self.columnsAssortment,
+                                    self.tableWidgetAssortment,
+                                    self.tabledataAssortment)
+            except Exception as e:
+                self.errorMessage(str(e))
+            self.saved = False
+        else:
+            self.delAssortment()
+
+    def delAssortment(self):
         try:
-            self.db.delRecordsByName(name)
-        except Exception as error:
-            self.errorMessage(str(error))
-        self.setDataToTable(self.db.getRecords())
-        self.saved = False
+            for i in self.tableWidgetAssortment.selectedIndexes():
+                print(self.tabledataAssortment)
+                self.db.deleteItem(self.tabledataAssortment[i.row()]['id'])
+                self.tabledataAssortment = self.db.getAssortment()
+                self.setDataToTable(self.columnsAssortment,
+                                    self.tableWidgetAssortment,
+                                    self.tabledataAssortment)
+        except Exception as e:
+            self.errorMessage(str(e))
+
+    def delOrders(self):
+        try:
+            for i in self.tableWidgetOrders.selectedIndexes():
+                print(self.tabledataOrders)
+                self.db.deleteOrder(self.tabledataOrders[i.row()]['id'])
+                self.tabledataOrders = self.db.getOrders()
+                self.setDataToTable(self.columnsOrders, self.tableWidgetOrders,
+                                    self.tabledataOrders)
+        except Exception as e:
+            self.errorMessage(str(e))
